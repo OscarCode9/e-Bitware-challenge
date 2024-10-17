b082c65ad0f012a5cb34d8d6966a26da1c8caf56
Oscar Martinez Martinez <oscar.99.tris@gmail.com>
2024-10-17 04:14:06 -0600

    fix debug and add handler global error

---
+++ src/main/java/com/usercrud/usercrud/Users/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.usercrud.usercrud.Users;
+
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConfigDataResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ConfigDataResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
